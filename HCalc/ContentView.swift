d4077f94cf4903ba15cc1b5fa610ecbdb7624e61
AWV2804 <atharvarao100@gmail.com>
2024-03-09 16:41:37 -0700

    Initial Commit

---
+++ HCalc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HCalc
+//
+//  Created by Atharva Rao on 3/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
