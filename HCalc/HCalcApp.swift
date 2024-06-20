6b566d775abe6e1ecd79005a3ba642f81e9e8a5f
Atharva Rao <atharvarao100@gmail.com>
2024-06-19 20:02:52 -0500

    Initial Commit

---
+++ HCalc/HCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  HCalcApp.swift
+//  HCalc
+//
+//  Created by Atharva Rao on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct HCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
